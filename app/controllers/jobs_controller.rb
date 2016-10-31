2b690d607a2a1072593569f6c9a2420281145b06
Bruno e Flavio <edson.silva@vagas.combr>
2016-10-31 20:06:34 -0200

    add new feature create job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,4 +3,15 @@ class JobsController < ApplicationController
       id = params[:id]
       @job = Job.find(id)
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    new_job = params.require(:job).permit(:title, :location, :category, :company, :description)
+    redirect_to job_url(new_job)
+
+
+  end
 end
