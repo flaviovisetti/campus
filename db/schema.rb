95d777261f3b937877350ee8cc2d9e9a042e60d2
Bruno e Flavio <edson.silva@vagas.combr>
2016-10-31 21:43:40 -0200

    add new migrate and feature DESTAQUE

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027223353) do
+ActiveRecord::Schema.define(version: 20161031231141) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161027223353) do
     t.string   "location"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "featured"
   end
 
 end
