965ced3a11e76d49eb1d91df469485e35872bddc
Bruno e Flavio <edson.silva@vagas.combr>
2016-11-01 19:49:32 -0200

    add new model company

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -9,8 +9,8 @@ class JobsController < ApplicationController
   end
 
   def create
-    new_job = params.require(:job).permit(:title, :location, :category, :company, :description)
-    @job = Job.new(title: new_job[:title], location: new_job[:location], category: new_job[:category], company: new_job[:company], description: new_job[:description])
+    new_job = params.require(:job).permit(:title, :location, :category, :company_id, :description)
+    @job = Job.new(title: new_job[:title], location: new_job[:location], category: new_job[:category], company_id: new_job[:company_id], description: new_job[:description])
     @job.save
     redirect_to job_url(@job)
 
