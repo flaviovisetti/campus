e92f396c6bf35e392a38e23eeb70be34d8b52d80
Flavio Visetti <flavio.visetti@gmail.com>
2016-11-02 00:08:59 -0200

    added new feature criar vagas corretamente

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
 class JobsController < ApplicationController
+
   def show
       id = params[:id]
       @job = Job.find(id)
@@ -12,7 +13,12 @@ class JobsController < ApplicationController
     new_job = params.require(:job).permit(:title, :location, :category, :company_id, :description)
     @job = Job.new(title: new_job[:title], location: new_job[:location], category: new_job[:category], company_id: new_job[:company_id], description: new_job[:description])
     @job.save
-    redirect_to job_url(@job)
+
+    if @job.save
+      redirect_to job_url(@job)
+    else
+      redirect_to new_job_path, flash: {referal_msg: 'Não foi possível criar a vaga'}
+    end
 
   end
 end
