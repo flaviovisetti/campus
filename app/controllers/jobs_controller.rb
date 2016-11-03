1a87821c88d20261fe220ed663430487167a0f17
Bruno e Flavio <edson.silva@vagas.combr>
2016-11-03 18:44:44 -0200

    Add new feature edit job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -19,4 +19,20 @@ class JobsController < ApplicationController
     end
 
   end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    edit_job_params = params.require(:job).permit(:title, :location, :category, :description, :featured, :company_id)
+    @job = Job.new(edit_job_params)
+
+    if @job.save
+      redirect_to job_path(@job)
+    else
+      redirect_to edit_job_path, flash: { referral_code: 'Não foi possível atualizar a vaga' }
+    end
+  end
+
 end
