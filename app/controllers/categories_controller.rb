c7b3590241c7579cc65eba30e8d69aaba87a7e24
Bruno e Flavio <edson.silva@vagas.combr>
2016-11-08 21:38:40 -0200

    update tests category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,22 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    category_params = params.require(:category).permit(:name)
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to category_url(@category)
+    else
+      flash.now[:alert]= 'Não é possível cadastrar categoria'
+      render :new
+    end
+  end
+
+
 end
--- spec/features/user_create_categories_spec.rb
+++ spec/features/user_create_categories_spec.rb
@@ -8,9 +8,31 @@ feature 'User create category' do
     fill_in('Nome' , with: 'DBA')
     click_on('Salvar Categoria')
 
-    visit category_path
-
     expect(page).to have_content('DBA')
+
+  end
+
+  scenario 'successfully' do
+
+    visit new_category_path
+
+    click_on('Salvar Categoria')
+
+    expect(page).to have_content 'Não é possível cadastrar categoria'
+
+  end
+
+  scenario 'save two category with same name' do
+
+    category = Category.create(name: 'DBA')
+
+    visit new_category_path
+
+    fill_in('Nome', with: 'DBA')
+    click_on('Salvar Categoria')
+
+    expect(page).to have_content 'Não é possível cadastrar categoria'
+    expect(page).to have_content 'Name has already been taken'
 
   end
 end
