587fc446506c858a6580adea78c078613b7ef8d5
Bruno e Flavio <edson.silva@vagas.combr>
2016-11-10 20:24:53 -0200

    add new feature edit category

---
+++ spec/features/user_edit_category_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'editing category' do
+
+  scenario 'sucessfuly' do
+    category = Category.create(name: 'desenvolvedor')
+
+    visit edit_category_path(category)
+
+    fill_in('Nome', with: 'analista')
+
+    click_on('Atualizar')
+
+    expect(page).to have_content('analista')
+  end
+
+  scenario 'fill field in blank' do
+    category = Category.create(name: 'desenvolvedor')
+
+    visit edit_category_path(category)
+
+    fill_in('Nome', with: '')
+
+    click_on('Atualizar')
+
+    expect(page).to have_content('Não é possível atualiza categoria')
+  end
+
+end
